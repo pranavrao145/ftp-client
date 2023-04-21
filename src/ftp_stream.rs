a8ab2f4646d8c617eed7e9a180bf5a6cdcf9a91d
Pranav Rao <56097527+pranavrao145@users.noreply.github.com>
2023-04-21 00:23:14 -0400

    feat(login): implemented login feature

---
+++ src/ftp_stream.rs
@@ -0,0 +1,104 @@
+use std::error::Error;
+
+use tokio::{
+    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
+    net::TcpStream,
+    sync::Mutex,
+};
+
+pub struct FtpStream {
+    pub conn: Mutex<BufReader<TcpStream>>,
+}
+
+impl FtpStream {
+    /// Attempt to connect to the server with the given address. Return an error
+    /// if the connection is unsuccessful.
+    ///
+    /// * `addr`: the address to attempt to connect to
+    pub async fn connect(addr: &str) -> Result<FtpStream, Box<dyn Error>> {
+        let stream = TcpStream::connect(addr).await?;
+        let connection = FtpStream {
+            conn: Mutex::new(BufReader::new(stream)),
+        };
+
+        let mut line = String::new();
+        let mut conn = connection.conn.lock().await;
+
+        conn.read_line(&mut line).await?;
+
+        let status_code: String = line.chars().take(3).collect();
+        let status_code = status_code.parse::<i32>()?;
+
+        // if we were not able to connect to the server
+        if status_code != 220 {
+            return Err("failed to connect to server".into());
+        }
+
+        drop(conn);
+
+        Ok(connection)
+    }
+
+    /// Attempt to log in to the FTP server with the username and password provided. Return an
+    /// error if the login is unsuccessful.
+    ///
+    /// * `username`: the username with which to make the login attempt
+    /// * `password`: the password with which to make the login attempt
+    pub async fn login(&mut self, username: &str, password: &str) -> Result<(), Box<dyn Error>> {
+        let mut conn = self.conn.lock().await;
+
+        self.write_to_conn(format!("USER {}\r\n", username).as_str(), &mut conn)
+            .await?;
+
+        // get response status code to check if password needed
+        let next_response_code = self.get_next_response_code(&mut conn).await?;
+
+        // if a password is needed
+        if next_response_code == 331 {
+            self.write_to_conn(format!("PASS {}\r\n", password).as_str(), &mut conn)
+                .await?;
+        }
+
+        // get response status code to check if password needed
+        let next_response_code = self.get_next_response_code(&mut conn).await?;
+
+        // if the login is not successful
+        if next_response_code != 230 {
+            return Err(format!("login failed with error code {}", next_response_code).into());
+        }
+
+        Ok(())
+    }
+
+    /// Writes the given message to the TCP connection
+    ///
+    /// * `message`: the message to write
+    /// * `conn`: a mutable reference to the BufReader containing the TCPStream to write to
+    pub async fn write_to_conn(
+        &self,
+        message: &str,
+        conn: &mut BufReader<TcpStream>,
+    ) -> Result<(), Box<dyn Error>> {
+        let stream = conn.get_mut();
+        stream.write_all(message.as_bytes()).await?;
+        stream.flush().await?;
+
+        Ok(())
+    }
+
+    /// Returns the status code of the next message to be read in the stream
+    ///
+    /// * `conn`: a mutable reference to the BufReader containing the TCPStream to read from
+    pub async fn get_next_response_code(
+        &self,
+        conn: &mut BufReader<TcpStream>,
+    ) -> Result<i32, Box<dyn Error>> {
+        let mut line = String::new();
+        conn.read_line(&mut line).await?;
+
+        let status_code: String = line.chars().take(3).collect();
+        let status_code = status_code.parse::<i32>()?;
+
+        Ok(status_code)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use ftp_client::ftp_stream::FtpStream;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>> {
+    let mut stream = FtpStream::connect("192.168.88.250:21").await?;
+    stream.login("username", "password").await?;
+
+    Ok(())
 }
